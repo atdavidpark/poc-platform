4b1cbc4294294ff08906dface472d5d65974b615
gschmutz <guido.schmutz@trivadis.com>
2020-12-17 16:32:14 +0100

    Update DataSourceSimulator.java

--- impl/java/java-kafka-producer/src/main/java/com/trivadis/DataSourceSimulator.java
+++ impl/java/java-kafka-producer/src/main/java/com/trivadis/DataSourceSimulator.java
@@ -4,15 +4,19 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.trivadis.kafka.producer.KafkaProducerAvro;
 
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Iterator;
 
 public class DataSourceSimulator {
 
-    static void runSimulator(final String fileName, final int sendMessageCount, final int speedUpFactor) throws Exception {
-        FileReader filereader = new FileReader(fileName);
+    private void runSimulator(final String fileName, final int sendMessageCount, final int speedUpFactor) throws Exception {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("data/tng.csv");
+        Reader fileReader = new InputStreamReader(is);
 
         // create csvReader object with parameter filereader and parser
-        Iterator<ControlDataDO> iterator = new CsvToBeanBuilder(filereader)
+        Iterator<ControlDataDO> iterator = new CsvToBeanBuilder(fileReader)
                 .withSeparator('\t')
                 .withType(ControlDataDO.class)
                 .build().iterator();
@@ -28,10 +32,11 @@ public class DataSourceSimulator {
     }
 
     public static void main(String... args) throws Exception {
+        DataSourceSimulator sim = new DataSourceSimulator();
         if (args.length == 0) {
-            runSimulator("/Users/gus/workspace/git/trivadispf/poc-platform/impl/java/java-kafka-producer/src/main/resources/data/tng.csv",10,0);
+            sim.runSimulator("/Users/gus/workspace/git/trivadispf/poc-platform/impl/java/java-kafka-producer/src/main/resources/data/tng.csv",10,0);
         } else {
-            runSimulator("data/test.csv", Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+            sim.runSimulator("data/test.csv", Integer.parseInt(args[0]),Integer.parseInt(args[1]));
         }
     }
 }
